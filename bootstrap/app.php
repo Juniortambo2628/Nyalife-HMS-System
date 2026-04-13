af53cfb1a92fc4e83f9db54db624f7402a8cc8b4
Juniortambo2628 <Juniortambo2628@gmail.com>
2026-04-13 15:28:25 +0300

    fix: Correct public path override logic in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
 
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Override public path for cPanel/Production environment
+if (file_exists('/home1/nyalifew/public_html')) {
+    $app->usePublicPath('/home1/nyalifew/public_html');
+}
+
+return $app;
